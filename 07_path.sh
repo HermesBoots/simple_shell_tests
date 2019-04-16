4918240fc8bfe511f9ba4d38628f6802d5b002c6
Nate Petersen <nate.petersen@me.com>
2019-04-16 16:47:15 -0400

    Update 07_path.sh (#20)
    * Update 07_path.sh
    
    * Use chmod on file that user has permissions for

--- 07_path.sh
+++ 07_path.sh
@@ -3,3 +3,23 @@
 source ./test.sh
 
 runtest 'ls -l /tmp'
+
+cat >temp.sh <<EOF
+echo "I should print"
+echo "This too"
+EOF
+chmod +x temp.sh
+export PATH=:$PATH
+runtest 'temp.sh'
+
+runtest '/bin/notAthing'
+runtest '/usr/bin'
+
+touch ./cal
+runtest 'cal'
+runtest '/usr/bin/cal'
+rm ./cal ./temp.sh
+runtest 'cal'
+unset PATH
+runtest 'cal'
+runtest 'ls'
