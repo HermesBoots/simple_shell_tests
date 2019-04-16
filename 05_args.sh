d565055403d0e6e37ed84623ba5aba8c8a8a7a1c
Nate Petersen <nate.petersen@me.com>
2019-04-16 16:52:00 -0400

    Update 05_args.sh (#26)
    * Update 05_args.sh
    
    more tests
    
    * Use fmt instead of cowsay to avoid installing

--- 05_args.sh
+++ 05_args.sh
@@ -6,3 +6,13 @@ runtest '/bin/ls -l'
 runtest '/usr/bin/cal'
 runtest 'echo word1     word2'
 runtest '         /bin/ls'
+runtest 'echo /test'
+runtest 'ls --this-is-a-nonsense-argument-that-is-very-long-and-is-silly-but-is-testing-something-important-im-sure-but-youll-never-know -a'
+echo 'Extremely long line of text that will be formatted to multiple lines when the fmt command is used.' > fmt_test.txt
+runtest 'fmt -20 fmt_test.txt'
+rm fmt_test.txt
+runtest 'whereis fmt'
+runtest 'which /bin/ls'
+runtest '-la ls'
+runtest 'ls -l -a'
+runtest 'ls --help'
