a53524a3b79abb2b7c93e2940d9bc171abfba12d
Nate Petersen <nate.petersen@me.com>
2019-04-16 15:03:08 -0400

    Update 03_simple.sh
    remove args from existing tests (not supposed to handle args at this point) and added several more tests.

--- 03_simple.sh
+++ 03_simple.sh
@@ -2,6 +2,17 @@
 
 source ./test.sh
 
-runtest '/bin/ls -l'
-runtest '/bin/ls -a'
+runtest '/bin/cal'
+runtest '/bin/l'
 runtest '/bin/ls'
+
+runtest '   /usr/bin/cal'
+runtest 'usr/bin/cal'
+runtest '/usr/bin/whereis/'
+runtest './bin/ls'
+runtest ' /bin /ls'
+runtest '/bin/ls/ls'
+runtest './ls'
+runtest '/usr/bin/.'
+runtest '/usr/include'
+runtest '/usr/local/bin/*'
