b97e741ddfb2bc75c29c12aee2c838654f2ca681
Nate Petersen <nate.petersen@me.com>
2019-04-16 14:36:19 -0400

    Update 07_path.sh

--- 07_path.sh
+++ 07_path.sh
@@ -3,3 +3,20 @@
 source ./test.sh
 
 runtest 'ls -l /tmp'
+
+cat >temp.sh <<EOF
+echo "I should print"
+echo "This too"
+EOF
+chmod +x temp.sh
+export PATH=$PATH:
+runtest 'temp.sh'
+
+runtest '/bin/notAthing'
+runtest '/usr/bin'
+
+chmod -x /usr/bin/cal
+runtest 'cal'
+runtest '/usr/bin/cal'
+chmod +x /usr/bin/cal
+runtest 'cal'
