f8007cb9a9cdb1cd1dd115f23ea335e397e4215e
Nate Petersen <nate.petersen@me.com>
2019-04-16 16:43:03 -0400

    Update 14_separator.sh (#21)
    * Update 14_separator.sh
    
    additional testing for ;
    
    * Remove case that runs into bizarre sh edge case

--- 14_separator.sh
+++ 14_separator.sh
@@ -5,3 +5,7 @@ source ./test.sh
 runtest 'pwd ; ls'
 runtest 'ls ; ls'
 runtest 'echo hello ; ls'
+runtest 'ls- ; echo; cal'
+runtest 'ti;me ; time'
+runtest 'where;is echo'
+runtest 'whereis echo ;'
